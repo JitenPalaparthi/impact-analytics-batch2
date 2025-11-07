f7d201763cb7f2d92ed588c1c02f5eb2ac237831
Jiten <jpalaparthi@gmail.com>
2025-11-07 11:33:53 +0530

    add more examples

--- 13-array-box/src/main.rs
+++ 13-array-box/src/main.rs
@@ -8,12 +8,41 @@ fn main() {
 
     println!("ptr:{:p} box ptr:{:p} ", ptr, &arr_box);
 
-    let num_box = Box::new(100); // stack allocation
-    println!("{:p}", &num_box);
-    let r = get_Sq(num_box);
+    let mut num_box = Box::new(10); // stack allocation
+    println!("ptr of box {:p} raw_ptr:{:p}", &num_box,num_box);
+   
+    num_box = get_Sq3(num_box);
+    println!("ptr of box {:p} raw_ptr:{:p}", &num_box,num_box);
+    // Box has not implemented copy // for the raw pointer
+    get_Sq1(&mut num_box);
+    println!("{}",*num_box);
+
+    get_Sq2(num_box); // The ownership is moved --> Traits
+   // println!("{}",*num_box);
+    // Box has implemented Copy
+
+
+    // let s1: String = "hello Wrold".to_string();
+    // let s2: &String = &s1; // Ownership is move or transfer fo the data
+    // println!("{}",s1)
+
+    // They consider the box is purely heap allocation
+
 }
 
-fn get_Sq(num: Box<i32>) -> i64 { // The address on stack is different on this stack frame
-    println!("{:p}", &num);
-    0
+fn get_Sq1(mut num: &mut Box<i32>) { // The address on stack is different on this stack frame
+    println!("ptr of box {:p} raw_ptr:{:p}", &num,num);
+    **num = **num * **num;
 }
+
+fn get_Sq2(mut num: Box<i32>) { // The address on stack is different on this stack frame
+    println!("ptr of box {:p} raw_ptr:{:p}", &num,num);
+    *num = *num * *num;
+}
+
+fn get_Sq3(mut num: Box<i32>)->Box<i32> { // The address on stack is different on this stack frame
+    println!("ptr of box {:p} raw_ptr:{:p}", &num,num);
+    *num = *num * *num;
+    return num
+}
+
