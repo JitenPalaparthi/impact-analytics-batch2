da77273a22535ece74c334f0bcf05019ea9f76fc
Jiten <jpalaparthi@gmail.com>
2025-11-07 10:57:04 +0530

    add ptr on box

--- 13-array-box/src/main.rs
+++ 13-array-box/src/main.rs
@@ -1,14 +1,19 @@
 fn main() {
-  
-  // let arr1 = [10,20,30,40,50];
+    // let arr1 = [10,20,30,40,50];
 
-  let arr_box = Box::new([10,20,30,40,50]); // Box is a smart pointer
+    let arr_box = Box::new([10, 20, 30, 40, 50]); // Box is a smart pointer
 
-  let ptr = arr_box.as_ptr();
-  // Try to do the ptr arithmetic 
+    let ptr = arr_box.as_ptr();
+    // Try to do the ptr arithmetic
 
-  println!("ptr:{:p} box ptr:{:p} ",ptr,&arr_box);
+    println!("ptr:{:p} box ptr:{:p} ", ptr, &arr_box);
 
+    let num_box = Box::new(100); // stack allocation
+    println!("{:p}", &num_box);
+    let r = get_Sq(num_box);
 }
 
-// 
+fn get_Sq(num: Box<i32>) -> i64 { // The address on stack is different on this stack frame
+    println!("{:p}", &num);
+    0
+}
